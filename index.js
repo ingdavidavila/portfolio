1966ab754b0be5b9030b6f1f4cb68b9a6e1b7123
David Avila <ingdavidavila@hotmail.com>
2022-06-01 13:40:37 -0500

    latest commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function picSize() {
+    document.getElementById("profile-pic").src= "profile pic 2.jpg"
+}
+
+function regSize() {
+    document.getElementById("profile-pic").src= "profile pic.jpg"
+}
